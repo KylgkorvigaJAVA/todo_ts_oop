5a9b384c55d8772e82d566ef2c77a4f8eedce1ac
Kaspar <kylgorvigajava@gmail.com>
2025-01-17 10:38:47 +0200

    Create Project basic files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import todoRoutes from './routes/todos'
+import exp from 'constants'
+
 const app = express()
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.json({message: error.message })
+})
+
 
 app.listen(3011, () => {
     console.log('Server is running at http://localhost:3011')
 })
 
-// kasuta "tsc" asemel - "tsc.cmd"
+// ###!!! kasuta terminalis commandi "tsc" asemel - "tsc.cmd" !!!###
