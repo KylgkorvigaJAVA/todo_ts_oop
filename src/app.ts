d589255902ee980a4b54cf237c11219921f13e88
Kaspar <kylgorvigajava@gmail.com>
2025-01-17 11:20:55 +0200

    Create new ToDo from user data

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express'
 import todoRoutes from './routes/todos'
-import exp from 'constants'
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/todos', todoRoutes)
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.json({message: error.message })
